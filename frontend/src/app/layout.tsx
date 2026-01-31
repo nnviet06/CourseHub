3cacd6824028db4409e522fc6db9b05efc18fafa
Will Doan <phongthedoan@usf.edu>
2026-01-31 18:22:17 -0500

    fixed file structure

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,13 @@
+const AppLayout = ( { children } : { children: React.ReactNode }) => {
+  return (
+    <html>
+      <body>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
+
+export default AppLayout;
