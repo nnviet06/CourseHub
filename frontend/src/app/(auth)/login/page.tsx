818dbe6fe15c37b0d59049d7b2327ddffbc67d4a
Will Doan <pdoanthe96@gmail.com>
2026-03-13 22:20:21 -0400

    updated login

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,80 +1,28 @@
 "use client";
-import styles from "./Auth.module.css";
+import styles from '../Auth.module.css'
+import { useState } from 'react'
+import EmailField from '../../../components/(auth)/EmailField'
+import PasswordField from '../../../components/(auth)/PasswordField'
 
+export default function Login() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
+  const handleLogin = () => {
+    console.log('User login');
+  }
 
-export default function SignInPage() {
   return (
-    <div className="login">
-      {/* <img */}
-      {/*   src="https://www.letseatcake.com/wp-content/uploads/2021/07/funny-memes-13.jpg" */}
-      {/*   alt="login image" */}
-      {/*   className="login__img" */}
-      {/* /> */}
-
-      <form className="login__form">
-        <h1 className="login__title">login</h1>
-
-        <div className="login__content">
-          {/* Username */}
-          <div className="login__box">
-            <i className="bx bx-lock-alt"></i>
-
-            <div className="login__box-input">
-              <input
-                type="email"
-                required
-                className="login__input"
-                placeholder=""
-              />
-              <label className="login__label">Username</label>
-            </div>
-          </div>
-
-          {/* Password */}
-          <div className="login__box">
-            <i className="ri-lock-2-line login__icon"></i>
-
-            <div className="login__box-input">
-              <input
-                type="password"
-                required
-                className="login__input"
-                id="login-pas"
-                placeholder=""
-              />
-              <label className="login__label">Password</label>
-              <i className="ri-eye-off-line login__eye"></i>
-            </div>
-          </div>
-        </div>
-
-        {/* Remember + forgot */}
-        <div className="login__check">
-          <div className="login__check-group">
-            <input
-              type="checkbox"
-              className="login__check-input"
-            />
-            <label className="login__check-label">
-              Remember me
-            </label>
-          </div>
-
-          <a href="#" className="login__forgot">
-            Forgot Password?
-          </a>
-        </div>
-
-        <button type="submit" className="login__button">
-          Login
-        </button>
-
-        <p className="login__register">
-          Don&apos;t have an account? <a href="#">Register</a>
+    <div className={styles.Login}>
+      <form className={styles.LoginForm}>
+        <h1>Log In</h1>
+        <EmailField value={email} onChange={(e) => setEmail(e.target.value)} />
+        <PasswordField label="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="button" onClick={handleLogin}>Log In</button>
+        <p>
+          Don&apos;t have an account? <a href="/signup">Sign Up</a>
         </p>
       </form>
     </div>
-  );
+  )
 }
-
