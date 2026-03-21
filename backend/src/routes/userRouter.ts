d7988821214c1de0a0bbab5358594ba80ca3924a
nnviet06 <nnviet2016@gmail.com>
2026-03-20 23:51:23 -0400

    refactor: move routes from userController to userRouter, add error handling and validation

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,10 +1,11 @@
 // user routes
 
 import express from 'express'
-import { getAllUsers } from '../controllers/userController'
+import { getAllUsers, createUser } from '../controllers/userController'
 
 const userRouter = express.Router()
 
 userRouter.get('/', getAllUsers)
+userRouter.post('/', createUser)
 
 export default userRouter
